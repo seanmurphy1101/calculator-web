b4e42f7473158ba36e2f7f733702f7ffa07fcdf9
Sean Murphy <seanmurphy1101@gmail.com>
2021-05-31 21:08:15 -0400

    added deciaml functionality and allowed delete to work on results of an expression

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let exp="";
 let val = [];
+let isDec1 = false;
+let isDec2 = false;
+let dec1 = 0;
+let dec2 = 0;
 let op = "";
 
 function addExp(arg1, arg2){
@@ -26,6 +30,10 @@ function raiseExp(arg1, arg2){
 }
 
 function operate(){
+    if (val[1]===undefined){
+        return;
+    }
+    let res;
     if (op==="+"){
         res = addExp(val[0],val[1]);
     }
@@ -43,7 +51,15 @@ function operate(){
     }
     clearBar();
     document.getElementById("bar").innerHTML = res;
-    updateExp(res);
+    if (res.toString().includes('.')){
+        isDec1 = true;
+        dec1 = res.toString().length - res.toString().indexOf('.')-1;
+        exp = res.toString();
+        val[0]=res;
+    }
+    else {
+        updateExp(res);
+    }
 }
 
 function updateExp(a){
@@ -51,28 +67,65 @@ function updateExp(a){
     if (typeof(a)==="number"){
         // if it is first val add to val[0]
         if (op===""){
-            // if val has no val, set it to arg
-            if (val.length===0){
+            if (isDec1){
+                if (a===0){
+                    dec1+=1;
+                    exp+="0";
+                    document.getElementById("bar").innerHTML = exp;
+                    return;
+                }
+                else {
+                    console.log(val[0]);
+                    val[0]=val[0]+a*Math.pow(10, -dec1-1);
+                    val[0]=parseFloat(val[0].toFixed(dec1+1));
+                    dec1+=1;
+                }
+            }
+            else if (val[0]===undefined){
                 val[0]=a;
             }
-            // if val does have a value, this has to be the new ones digit
             else {
                 val[0]=val[0]*10+a;
             }
-            // update exp with new first value
             exp = val[0].toString();
         }
         // if it is a number, but op is already defined, then we must update val[1]
         else {
-            // if val has no val, set it to arg
-            if (val[1]===undefined){
+            if (isDec2){
+                if (a===0){
+                    dec2+=1;
+                    exp+="0"
+                    document.getElementById("bar").innerHTML = exp;
+                }
+                else {
+                    val[1]=val[1]+a*Math.pow(10, -dec2-1);
+                    val[1]=parseFloat(val[1].toFixed(dec2+1));
+                    dec2+=1;
+                }
+            }
+            else if (val[1]===undefined){
                 val[1]=a;
             }
-            // if val does have a value, this has to be the new ones digit
             else {
                 val[1]=val[1]*10+a;
             }
             exp = val[0].toString()+op+val[1].toString();
+        }
+    }
+    else if (a==="."){
+        if (val.length!==0){
+            if (op===""){
+                if (!isDec1){
+                    isDec1 = true;
+                    exp = val[0]===undefined ? "." : val[0].toString()+".";
+                }
+            }
+            else {
+                if (!isDec2 && val[1]!==undefined){
+                    isDec2 = true;
+                    exp = val[1]===undefined ? val[0].toString()+op+"." : val[0].toString()+op+val[1].toString()+".";
+                }
+            }
         }
     }
     else {
@@ -89,14 +142,22 @@ function clearBar(){
     exp ="";
     val = [];
     op = "";
+    isDec1 = false;
+    isDec2 = false;
+    dec1 = 0;
+    dec2=0;
     document.getElementById("bar").innerHTML = exp;
     
 }
 
 function deleteItem(){
-    exp = exp.slice(0, exp.length-1);
+    exp = exp.slice(0, exp.length-1);   
     if (op===""){
-        if (val.length>0){
+        if (isDec1){
+            val[0]=parseFloat(exp);
+            dec1-=1;
+        }
+        else if (val.length>0){
             if (val[0].toString().length>1){
                 val[0]=(val[0]-(val[0]%10))/10;
             }
@@ -109,7 +170,26 @@ function deleteItem(){
         op = "";
     }
     else {
-        val[1]=(val[1]-(val[1]%10))/10;
+        if (isDec2){
+            let index = exp.indexOf(op);
+            let temp = exp.slice(index+1, exp.length)
+            val[1]=parseFloat(temp);
+            dec2-=1;
+        }
+        if (val[1].toString().length>1){
+            val[1]=(val[1]-(val[1]%10))/10;
+        }
+        else {
+            val[1] = undefined;
+        }
+    }
+    if (dec1===-1){
+        isDec1=false;
+        dec1=0;
+    }
+    if (dec2===-1){
+        isDec2=false;
+        dec2=0;
     }
     document.getElementById("bar").innerHTML = exp;
 }
